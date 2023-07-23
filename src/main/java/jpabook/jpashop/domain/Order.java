085550e6e55977508d91253c4b765d6079fc9ed3
ChoiWonYu <phillip07290@naver.com>
2023-07-23 16:40:05 +0900

    feat: Order Entity

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,49 @@
+package jpabook.jpashop.domain;
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+
+  @Id
+  @GeneratedValue
+  @Column(name = "order_id")
+  private long id;
+
+  @Enumerated(EnumType.STRING)
+  private OrderStatus status;
+
+  @OneToMany(mappedBy = "order",cascade = CascadeType.ALL)
+  private List<OrderItem> orderItems= new ArrayList<>();
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member member;
+
+  @OneToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+  @JoinColumn(name="delivery_id")
+  private Delivery delivery;
+
+  private LocalDateTime orderDate;
+}
