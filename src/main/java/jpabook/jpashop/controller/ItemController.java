f3c54351e79f266c8eab16e038a3fccf8eba6021
ChoiWonYu <phillip07290@naver.com>
2023-07-28 17:02:48 +0900

    feat: book createdto 유효성 검사

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.controller;
 
+import jakarta.validation.Valid;
 import java.util.List;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.dto.item.BookCreateRequestDto;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequiredArgsConstructor
@@ -24,12 +26,12 @@ public class ItemController {
   }
 
   @PostMapping(value = "/items")
-  public Item createBook(BookCreateRequestDto bookCreateRequestDto) {
+  public Item createBook(@Valid @RequestBody BookCreateRequestDto bookCreateRequestDto) {
     return itemService.createBook(bookCreateRequestDto);
   }
 
   @PutMapping(value = "/items/{itemId}")
-  public Item updateBook(@PathVariable final Long itemId, BookUpdateRequestDto bookUpdateRequestDto) {
+  public Item updateBook(@PathVariable final Long itemId,@RequestBody BookUpdateRequestDto bookUpdateRequestDto) {
     return itemService.updateBook(itemId,bookUpdateRequestDto);
   }
 
--- src/main/java/jpabook/jpashop/dto/item/BookCreateRequestDto.java
+++ src/main/java/jpabook/jpashop/dto/item/BookCreateRequestDto.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.dto.item;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,10 +10,18 @@ import lombok.Setter;
 @Setter
 public class BookCreateRequestDto {
 
+  @NotEmpty(message = "책 이름은 필수입니다.")
   private String name;
+
+  @NotEmpty(message = "가격은 필수입니다.")
   private int price;
+
+  @NotEmpty(message = "책 수량은 필수입니다.")
   private int stockQuantity;
+
+  @NotEmpty(message = "저자는 필수입니다.")
   private String author;
+
   private String isbn;
 
 }
